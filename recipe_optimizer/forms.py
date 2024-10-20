c9b8f9c3c4ca7aec3caf04036238d60ddc1b31c3
Furkan Baytak <furkanbaytak@hotmail.com>
2024-10-20 22:34:08 +0300

    Checklist for Prerequisites

--- recipe_optimizer/forms.py
+++ recipe_optimizer/forms.py
@@ -26,9 +26,8 @@ class RecipeStepForm(forms.ModelForm):
             'occupies_chef': forms.CheckboxInput(attrs={
                 'class': 'form-check-input'
             }),
-            'prerequisites': forms.SelectMultiple(attrs={
-                'class': 'form-select',
-                'multiple': 'multiple'
+            'prerequisites': forms.CheckboxSelectMultiple(attrs={
+                'class': 'form-check'
             }),
         }
 
@@ -36,7 +35,7 @@ class RecipeStepForm(forms.ModelForm):
         recipe = kwargs.pop('recipe')  # Extract the recipe from kwargs
         super().__init__(*args, **kwargs)
         self.fields['occupies_chef'].label = "Requires Chef"  # Checkbox label
-        self.fields['prerequisites'].label = "Prerequisites"  # Dropdown label
+        self.fields['prerequisites'].label = "Prerequisites"  # Checkbox label
 
         # Filter prerequisites to only show those related to the given recipe
         self.fields['prerequisites'].queryset = RecipeStep.objects.filter(recipe=recipe)
